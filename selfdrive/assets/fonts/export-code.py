3160dd381992bb7c6036fb96f682b3b77a93e50d
David T <dt052901@gmail.com>
2025-10-28 13:01:19 -0500

    refactor

--- selfdrive/assets/fonts/export-code.py
+++ selfdrive/assets/fonts/export-code.py
@@ -4,14 +4,11 @@ import os
 
 def main():
   os.environ["EXPORT_FONTS_AS_C"] = "1"
-
   from openpilot.system.ui.lib.application import gui_app
 
   gui_app.init_window("Exporting fonts")
   for _ in gui_app.render():
     pass
-
-  print("Done")
 
 
 if __name__ == "__main__":
