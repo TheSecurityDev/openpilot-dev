7a807bdd0acafc118ed98307520ba1cac1adac71
David <dt052901@gmail.com>
2026-02-24 00:33:10 -0600

    fix and add network

--- selfdrive/ui/tests/diff/replay_script.py
+++ selfdrive/ui/tests/diff/replay_script.py
@@ -219,34 +219,39 @@ def build_mici_script(pm: PubMaster, main_layout, script: Script) -> None:
   swipe_right(width, wait_after=WAIT_SHORT)  # back to home
 
   def explore_panel(item_count: int, interact_fn: Callable[[int], None] | None = None, get_swipe_amount: Callable[[int], tuple[int, int]] | None = None):
-    """Helper function to explore a panel with the given number of items by swiping through each one and
-    optionally interacting with them using the provided callback. If `get_swipe_amount` is provided, it will be called
-    for each item index to determine the swipe distance and duration for that item, allowing for variable spacing between items.
-    Otherwise, a default swipe amount will be used for all items. Swipes back to the beginning once completed.
+    """Helper function to explore a panel with the given number of items by swiping through each one
+    and optionally interacting with them using the provided callback.
+      If `get_swipe_amount` is provided, it will be called for each item index to determine the swipe distance and duration
+    for that item, allowing for variable spacing between items. Otherwise, a default swipe amount will be used for all items.
     """
-    SWIPE_AMOUNT: tuple[int, int] = (210, 10)  # distance, duration
-    total_distance = 0
+    SWIPE_AMOUNT = (210, 10)  # distance, duration
+    distance = 0  # track cumulative distance so we can swipe back
     # Scroll settings and back
     for i in range(item_count):
-      # test trying to scroll past the last item, so skip clicking again
+      # tests trying to scroll past the last item, so skip clicking again
       if (i < item_count):
         if interact_fn:
           interact_fn(i)
       # swipe to roughly the center of the next toggle (using the provided get_swipe_amount callback if necessary)
       swipe_amount = (get_swipe_amount(i) or SWIPE_AMOUNT) if get_swipe_amount else SWIPE_AMOUNT
-      total_distance += swipe_amount[0]
+      distance += swipe_amount[0]
       swipe_left(*swipe_amount)
-    swipe_right(SWIPE_AMOUNT[0] * item_count, SWIPE_AMOUNT[1])  # go back to beginning
+    swipe_right(distance * item_count, SWIPE_AMOUNT[1])  # go back to beginning
 
   def interact_toggles(i: int):
     # go through all toggle states (first one is personality, which has 3 states)
     for _ in range(3 if i == 0 else 2):
       click(wait_after=FAST_CLICK)  # click through each toggle quickly
 
+  def interact_network(i: int):
+    pass
+
   def interact_settings(i: int):
     click()  # click each setting
     if (i == 0):
       explore_panel(8, interact_toggles)  # explore toggles
+    elif (i == 1):
+      explore_panel(4, interact_network)  # explore network settings
     swipe_down()  # go back
 
   # === Settings === #
